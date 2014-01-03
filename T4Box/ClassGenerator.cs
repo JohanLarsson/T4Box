4eb370c0cc56db2e6e7ce72b734ede1c0fff9592
Johan Larsson <johan_gavle1@hotmail.com>
2014-01-03 20:01:30 +0100

    Initial

---
+++ T4Box/ClassGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace T4Box
+{
+    public class ClassGenerator
+    {
+        [Test]
+        public void WriteClassWithoutArgumentsTest()
+        {
+            var template = new SimpleClassTemplate();
+            Console.Write(template.TransformText());
+        }
+    }
+}
