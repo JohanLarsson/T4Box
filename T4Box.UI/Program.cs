014a829fb7c1b766c542badc19f67c66910fc953
stevelikesbeer <stevegranet@gmail.com>
2014-01-12 21:12:22 -0500

    Steves first T4 generation added

--- T4Box.UI/Program.cs
+++ T4Box.UI/Program.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Templates;
 
-namespace T4Box.UI
+namespace T4Box
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            /* ----------- Using for loops in code generation -----------------
+            CountStuff generatedCode = new CountStuff();
+            generatedCode.DoCounter();
+            Console.ReadLine();
+            */
+
+
         }
     }
 }
