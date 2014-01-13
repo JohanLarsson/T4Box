42d5cb7759413f9d1c42ade4e7ca461a66e24438
stevelikesbeer <stevegranet@gmail.com>
2014-01-12 21:00:19 -0500

    Added a UI and created a simple T4 class

---
+++ Templates/StevesT4Test.cs
@@ -0,0 +1,16 @@
+﻿ 
+namespace T4Box 
+{
+	public class CountStuff
+	{
+		public void DoCounter()
+		{
+						System.Console.WriteLine(1);
+						System.Console.WriteLine(2);
+						System.Console.WriteLine(3);
+						System.Console.WriteLine(4);
+						System.Console.WriteLine(5);
+			
+		}
+	}
+}
